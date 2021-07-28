7f5199a2cef2401b5ca49085c2bc6311defc342e
Pranshu Dobhal <pranshu.dobhal@gmail.com>
2021-07-29 02:35:20 +0530

    updated icon, user suggestion and new post
    - Updated the favicon from redux to bubble
    - Added a toggle for user suggestion component
    - Updated the New Post button to show "Posting" while submitting

--- src/components/UserSuggestion.jsx
+++ src/components/UserSuggestion.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Avatar from 'react-avatar';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router';
@@ -6,33 +7,40 @@ export const UserSuggestion = () => {
   const users = useSelector((state) => state.search.users);
   const loggedInUser = useSelector((state) => state.authentication.user);
   const navigate = useNavigate();
+  const [isSuggestionsVisible, setIsSuggestionsVisible] = useState(false);
 
   const userNotFollowedByLoggedInUser = users.filter((user) => !loggedInUser.following.find((loggedInUserFollowing) => loggedInUserFollowing._id === user._id));
 
   return (
     <div className="my-10">
       <div className="text-black font-semibold text-lg mb-2">
-        <h1>Suggested for you</h1>
+        <h1 className="flex justify-between">
+          Suggested for you{' '}
+          <span className="cursor-pointer text-indigo-500 text-sm" onClick={() => setIsSuggestionsVisible((isSuggestionsVisible) => !isSuggestionsVisible)}>
+            {isSuggestionsVisible ? 'Hide' : 'Show'}
+          </span>
+        </h1>
       </div>
       <div className="flex flex-nowrap overflow-x-auto suggestions pb-4">
-        {userNotFollowedByLoggedInUser.slice(0, 5).map((user) => {
-          return (
-            user.username !== loggedInUser.username && (
-              <div className="bg-white overflow-hidden shadow-lg rounded-lg mr-6 min-w-15" key={user.username}>
-                <div className="text-center p-6 border-b">
-                  <Avatar name={user.firstName + ' ' + user.lastName} size="96" className="rounded-full h-24 w-24 object-cover object-center cursor-pointer mx-auto" alt={user.firstName} onClick={() => navigate(`/${user.username}`)} />
-                  {/* <img className="h-24 w-24 rounded-full mx-auto cursor-pointer" src={user.profileImageURL} alt={user.firstName} onClick={() => navigate(`/${user.username}`)} /> */}
-                  <p className="pt-2 text-lg font-semibold cursor-pointer" onClick={() => navigate(`/${user.username}`)}>
-                    {user.firstName + ' ' + user.lastName}
-                  </p>
-                  <p className="text-sm text-gray-500 text-opacity-80 cursor-pointer" onClick={() => navigate(`/${user.username}`)}>
-                    @{user.username}
-                  </p>
+        {isSuggestionsVisible &&
+          userNotFollowedByLoggedInUser.slice(0, 5).map((user) => {
+            return (
+              user.username !== loggedInUser.username && (
+                <div className="bg-white overflow-hidden shadow-lg rounded-lg mr-6 min-w-15" key={user.username}>
+                  <div className="text-center p-6 border-b">
+                    <Avatar name={user.firstName + ' ' + user.lastName} size="96" className="rounded-full h-24 w-24 object-cover object-center cursor-pointer mx-auto" alt={user.firstName} onClick={() => navigate(`/${user.username}`)} />
+                    {/* <img className="h-24 w-24 rounded-full mx-auto cursor-pointer" src={user.profileImageURL} alt={user.firstName} onClick={() => navigate(`/${user.username}`)} /> */}
+                    <p className="pt-2 text-lg font-semibold cursor-pointer" onClick={() => navigate(`/${user.username}`)}>
+                      {user.firstName + ' ' + user.lastName}
+                    </p>
+                    <p className="text-sm text-gray-500 text-opacity-80 cursor-pointer" onClick={() => navigate(`/${user.username}`)}>
+                      @{user.username}
+                    </p>
+                  </div>
                 </div>
-              </div>
-            )
-          );
-        })}
+              )
+            );
+          })}
       </div>
     </div>
   );
--- src/features/posts/NewPost.jsx
+++ src/features/posts/NewPost.jsx
@@ -1,32 +1,39 @@
 import { useState } from 'react';
+import Avatar from 'react-avatar';
 import { useDispatch, useSelector } from 'react-redux';
 import { addNewPost } from './postsSlice';
 
 export const NewPost = () => {
   const [content, setContent] = useState('');
   const {
-    user: { _id: userID },
+    user: { _id: userID, firstName, lastName },
   } = useSelector((state) => state.authentication);
   const postStatus = useSelector((state) => state.posts.status);
   const dispatch = useDispatch();
   const onContentChanged = (e) => setContent(e.target.value);
   const isNewPostButtonDisabled = postStatus !== 'loading' && content.trim() === '';
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
   const onNewPostClicked = () => {
     const trimmedContent = content.trim();
     if (trimmedContent !== '' && postStatus !== 'loading') {
       dispatch(addNewPost({ userID, content }));
+      setIsSubmitted(true);
       setContent('');
     }
+    isNewPostButtonDisabled && setIsSubmitted(false);
   };
 
   return (
     <section className="border border-gray-200 rounded-lg p-6 shadow-md bg-white">
-      <form className="flex flex-col">
-        <textarea className="h-20 mb-2 border-0 rounded resize-none" id="postContent" name="postContent" placeholder="What's happening?" value={content} onChange={onContentChanged} />
-        <button type="button" className="bg-blue-500 text-white rounded-lg w-max py-2 px-6 mt-2 self-end disabled:opacity-50" onClick={onNewPostClicked} disabled={isNewPostButtonDisabled}>
-          New Post
-        </button>
+      <form className="flex">
+        <Avatar name={firstName + ' ' + lastName} size="60" className="rounded-full object-cover object-center cursor-pointer" alt={firstName} />
+        <section className="ml-2 flex flex-col flex-1">
+          <textarea className="h-20 mb-2 border-0 rounded resize-none" id="postContent" name="postContent" placeholder="What's happening?" value={content} onChange={onContentChanged} />
+          <button type="button" className="bg-blue-500 text-white rounded-lg w-max py-2 px-6 mt-2 self-end disabled:opacity-50" onClick={onNewPostClicked} disabled={isNewPostButtonDisabled}>
+            {isSubmitted && postStatus === 'loading' ? 'Posting' : 'New Post'}
+          </button>
+        </section>
       </form>
     </section>
   );
